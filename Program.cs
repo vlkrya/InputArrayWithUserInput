50edac5da296f74bcc501db76a16390df332fb87
vlkrya <95353760+vlkrya@users.noreply.github.com>
2022-04-18 01:26:29 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ITMO.ArrayWithUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                int n = 0;
+                int[] T = new int[0];
+                while (true)
+                {
+                    string userInput = Console.ReadLine();
+                    if (userInput == "F")
+                    {
+                        break;
+                    }
+                    else if (userInput == "n")
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
